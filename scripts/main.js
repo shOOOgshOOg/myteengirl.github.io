cac90e3327146346ce2ef7656719c9e3fb4c47df
shOOOgshOOg <100538091+shOOOgshOOg@users.noreply.github.com>
2022-03-01 15:50:47 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -418,7 +418,7 @@ var trainees = [];
 var filteredTrainees = [];
 // holds the ordered list of rankings that the user selects
 var ranking = newRanking();
-const rowNums = [1, 2, 3, 6];
+const rowNums = [1, 2, 3, 5];
 //window.addEventListener("load", function () {
   populateRanking();
   readFromCSV("./trainee_info.csv");
